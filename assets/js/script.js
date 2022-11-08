63a2549f8e297568e7df4760c3283704b09b90a0
Isaiah Destine <isaiahdestine@gmail.com>
2022-11-07 20:38:16 -0500

    js work

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,31 @@ let cardRow = $(".cardRow");
 
 // Current date var
 var today = new Date();
-let dd = String(today.getDate()).padStart(2,"0");
-let mm = String(today.getMonth() + 1).padStart(2,"0");
+let dd = String(today.getDate()).padStart(2, "0");
+let mm = String(today.getMonth() + 1).padStart(2, "0");
 let yyyy = today.getFullYear();
 var today = mm + "/" + dd + "/" + yyyy;
+
+// Local storage for historyItems
+if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
+  console.log("searchHistory not found");
+  //if it does not return null render searchHistory
+} else {
+  console.log("searchHistory loaded into searchHistoryArr");
+  renderSearchHistory();
+}
+searchButton.on("click", function (e) {
+  e.preventDefault();
+  if (searchInput.val() === "") {
+    alert("You must enter a city");
+    return;
+  }
+  console.log("clicked button");
+  getWeather(searchInput.val());
+});
+$(document).on("click", ".historyEntry", function () {
+  console.log("clicked history item");
+  let thisElement = $(this);
+  getWeather(thisElement.text());
+});
+// search history detail per city
